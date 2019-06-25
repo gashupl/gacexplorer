bd4d2aa64d25d82e87ab74b9334d84b533ab2f6d
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2019-06-25 19:45:48 +0200

    Adding project to the solution

--- App/GacExplorer.UI/MainForm.Designer.cs
+++ App/GacExplorer.UI/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GacExplorer.UI
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
---
+++ Test/GacExplorer.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GacExplorer.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GacExplorer.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa4e023f-bd45-4029-8e54-cdc4269584ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
