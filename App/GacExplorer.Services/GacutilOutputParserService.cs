7728221bb3fb51ce069cd031f521f654d024ffab
Piotr Gaszewski <piog@netcompany.com>
2019-10-15 08:29:24 +0200

    ParseListOutput partial implementation

--- App/GacExplorer.Services/GacutilOutputParserService.cs
+++ App/GacExplorer.Services/GacutilOutputParserService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using GacExplorer.Services.DTO;
 using GacExplorer.Services.OperationResults;
 
 namespace GacExplorer.Services
@@ -7,7 +10,24 @@ namespace GacExplorer.Services
     {
         public GacutilOutputParserResult ParseListOutput(string output)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrWhiteSpace(output))
+            {
+                return new GacutilOutputParserResult(OperationResult.Failed, "GacutilOutputParserService.ParseListOutput operation failed. Empty output string"); 
+            }
+
+            var assemblyLinesList = new List<AssemblyLineDto>();
+
+            string[] outputLines = Regex.Split(output, "\r\n|\r|\n"); //Split every line
+            foreach(var line in outputLines)
+            {
+                string[] words = line.Split(','); //Not using Regex becuase it is not necessary and slower
+                if(words.Length == 5)
+                {
+                    //TODO: For every line meeting above mentioned condition - try to convert it into AssemblyLineDto
+                }
+            }
+
+            throw new NotImplementedException(); 
         }
     }
 }
