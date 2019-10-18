44776821d717639a7c1d79fa2083b1e5718ce862
Piotr Gaszewski <piog@netcompany.com>
2019-10-18 08:08:59 +0200

    Binding assembly list to grid view

--- App/GacExplorer.UI/MainForm.cs
+++ App/GacExplorer.UI/MainForm.cs
@@ -10,35 +10,27 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using GacExplorer.Services.OperationResults;
 using GacExplorer.CommandProxy;
+using GacExplorer.Services.DTO;
 
 namespace GacExplorer.UI
 {
     public partial class MainForm : Form
     {
         private IGacutilLocationService gacutilLocationService;
+        private IGacutilOutputParserService parserService;
+        private IGlobalAssemblyCacheService gacService; 
         private IGacutil gacUtilProxy; 
 
-        public MainForm(IGacutilLocationService configurationService)
+        public MainForm(IGacutilLocationService configurationService, IGacutilOutputParserService parserService)
         {
             InitializeComponent();
-            this.gacutilLocationService = configurationService; 
+            this.gacutilLocationService = configurationService;
+            this.parserService = parserService; 
         }
 
         private void MainForm_Shown(object sender, EventArgs e)
         {
-            var result = this.gacutilLocationService.Read();
-            if (result.Result == OperationResult.Success)
-            {
-                var location = result.Location;
-                if (this.gacutilLocationService.FileExists(location))
-                {
-                    this.gacUtilProxy = new Gacutil(location);
-                }
-                else
-                {
-                    ShowGacFileDialog();
-                }
-            }
+            InitializeGacUtilProxy(); 
         }
 
         private void ConfigureGacutilLocationToolStripMenuItem_Click(object sender, EventArgs e)
@@ -62,9 +54,34 @@ namespace GacExplorer.UI
             this.openGacFileDialog.Dispose();
         }
 
+        private void InitializeGacUtilProxy()
+        {
+            var result = this.gacutilLocationService.Read();
+            if (result.Result == OperationResult.Success)
+            {
+                var location = result.Location;
+                if (this.gacutilLocationService.FileExists(location))
+                {
+                    this.gacUtilProxy = new Gacutil(location);
+                }
+                else
+                {
+                    ShowGacFileDialog();
+                }
+            }
+        }
+
         private void ListAssembliesToolStripMenuItem_Click(object sender, EventArgs e)
         {
+         
+            if (gacUtilProxy == null)
+            {
+                InitializeGacUtilProxy();
+            }
 
+            this.gacService = new GlobalAssemblyCacheService(this.gacUtilProxy, this.parserService);
+            var bindingList = new BindingList<AssemblyLineDto>(gacService.GetAssemblyLines().AssemblyLines);
+            this.gridViewAssemblies.DataSource = new BindingSource(bindingList, null);
         }
     }
 }
