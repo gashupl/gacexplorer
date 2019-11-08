c6d60c954067e904c4a9e7f8db49efaa8b4a45e5
Piotr Gaszewski <piog@netcompany.com>
2019-11-08 08:40:52 +0100

    UnregiesterAssembly method implementation + Gacutil class refactoring

--- App/GacExplorer.CommandProxy/Gacutil.cs
+++ App/GacExplorer.CommandProxy/Gacutil.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GacExplorer.CommandProxy
 {
@@ -22,18 +17,7 @@ namespace GacExplorer.CommandProxy
             string output;
             using (Process gacutilProcess = new Process())
             {
-                gacutilProcess.StartInfo.UseShellExecute = false;
-
-                gacutilProcess.StartInfo.FileName = this.location;
-                gacutilProcess.StartInfo.Arguments = "-l";
-                gacutilProcess.StartInfo.CreateNoWindow = true;
-                gacutilProcess.StartInfo.UseShellExecute = false;
-                gacutilProcess.StartInfo.RedirectStandardOutput = true;
-                var result = gacutilProcess.Start();
-
-                StreamReader reader = gacutilProcess.StandardOutput;
-                output = reader.ReadToEnd();
-
+                output = StartProcess(gacutilProcess, "-l");
                 gacutilProcess.WaitForExit();
             }
 
@@ -43,23 +27,10 @@ namespace GacExplorer.CommandProxy
         public string RegisterAssembly(string path)
         {
             //Sample command for registering:  .\gacutil.exe /i C:\Users\piog\Source\GitHub\gacexplorer\Assets\GacExplorer.TestLibrary\bin\Debug\GacExplorer.TestLibrary.dll
-
             string output;
             using (Process gacutilProcess = new Process())
             {
-                gacutilProcess.StartInfo.UseShellExecute = false;
-
-                gacutilProcess.StartInfo.FileName = this.location;
-                gacutilProcess.StartInfo.Arguments = $"/i {path}";
-                gacutilProcess.StartInfo.CreateNoWindow = true;
-                gacutilProcess.StartInfo.UseShellExecute = false;
-                gacutilProcess.StartInfo.RedirectStandardOutput = true;
-                gacutilProcess.StartInfo.Verb = "runas";
-                var result = gacutilProcess.Start();
-
-                StreamReader reader = gacutilProcess.StandardOutput;
-                output = reader.ReadToEnd();
-
+                output = StartProcess(gacutilProcess, $"/i {path}", true); 
                 gacutilProcess.WaitForExit();
             }
 
@@ -69,7 +40,33 @@ namespace GacExplorer.CommandProxy
         public string UnregisterAssembly(string name)
         {
             //Sample command for unregister:  .\gacutil.exe /u GacExplorer.TestLibrary
-            throw new NotImplementedException();
+            string output;
+            using (Process gacutilProcess = new Process())
+            {
+                output = StartProcess(gacutilProcess, $"/u {name}", true);
+                gacutilProcess.WaitForExit();
+            }
+
+            return output;
+        }
+
+        private string StartProcess(Process gacutilProcess, string arguments, bool runas = false)
+        {
+            gacutilProcess.StartInfo.UseShellExecute = false;
+            gacutilProcess.StartInfo.FileName = this.location;
+            gacutilProcess.StartInfo.Arguments = arguments;
+            gacutilProcess.StartInfo.CreateNoWindow = true;
+            gacutilProcess.StartInfo.UseShellExecute = false;
+            gacutilProcess.StartInfo.RedirectStandardOutput = true;
+
+            if (runas)
+            {
+                gacutilProcess.StartInfo.Verb = "runas";
+            }
+
+            var result = gacutilProcess.Start();
+            StreamReader reader = gacutilProcess.StandardOutput;
+            return reader.ReadToEnd();
         }
 
 
