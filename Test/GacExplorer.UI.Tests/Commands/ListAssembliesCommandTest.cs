cb88c9ebf9c7eb1857d8dfff558c875b81d3cf2d
Piotr Gaszewski <piog@netcompany.com>
2020-04-06 08:28:35 +0200

    New test class ListAssembliesCommandTest added

---
+++ Test/GacExplorer.UI.Tests/Commands/ListAssembliesCommandTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GacExplorer.UI.Tests.Commands
+{
+    [TestClass]
+    public class ListAssembliesCommandTest
+    {
+        [TestMethod]
+        public void Execute_EmptyLocationString_InvokeInitializeGacUtilProxyCommand()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        [TestMethod]
+        public void Execute_GacServiceNotExists_InitializeGacService()
+        {
+            //TODO: Possible refactoring - use factory to initialize Gac Service
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Execute_AssemblyLineListIsNull_DoNotSetDataSourceAndAssemblyCount()
+        {
+            //TODO: Possible refactoring - separate commands for setting data source and assembly count
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Execute_AssemblyLineListIsNotNull_SetDataSourceAndAssemblyCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
